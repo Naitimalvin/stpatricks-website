583fbb4073c563898b71c8df2e62515e9f7aa416
Naitimalvin <99694836+Naitimalvin@users.noreply.github.com>
2025-07-11 00:47:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// You can add interactive JS here later
+
+// Toggle mobile menu
+function toggleMenu() {
+  const nav = document.querySelector("header nav ul");
+  nav.classList.toggle("show");
+}
+
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
